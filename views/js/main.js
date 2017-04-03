af690f6b57e76baea0f7d55aee2ea13b1bcd7b56
andreakpresley <andreapresley5@gmail.com>
2017-04-03 15:50:01 -0500

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -504,7 +504,7 @@ function updatePositions() {
   var items = document.querySelectorAll('.mover');
   var phase = Math.sin((document.body.scrollTop / 1250);
   for (var i = 0; i < items.length; i++) {
-    phase = phase + (i%5);
+    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5);
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
